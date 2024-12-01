873ee8b59a96c2e4d9ac72537e5b13376ee85f23
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-01 05:23:58 -0700

    chore: Finish Day1

---
+++ Day1/Puzzle/Puzzle.cs
@@ -0,0 +1,54 @@
+﻿namespace CaptainCoder.AdventOfCode;
+
+public class Puzzle
+{
+    public string Input { get; }
+
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public static (List<int> Left, List<int> Right) SanitizeInput(string input)
+    {
+        string[] rows = input.Split(Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        List<int> left = new();
+        List<int> right = new();
+        foreach (string row in rows)
+        {
+            string[] items = row.Split("   ");
+            left.Add(int.Parse(items[0]));
+            right.Add(int.Parse(items[1])); 
+        }
+        return (left, right);
+    }
+
+    public virtual string Part1()
+    {
+        (List<int> left, List<int> right) = SanitizeInput(Input);
+        left.Sort();
+        right.Sort();
+        // [1, 2, 3, 4, 5]
+        // [5, 7, 2, 9, 8]
+        // Zip => [(1, 5), (2, 7), (3, 2), (4, 9), (5, 8)]
+        return left.Zip(right).Sum(p => Math.Abs(p.First - p.Second)).ToString();
+    }
+
+    public virtual string Part2()
+    {
+        (List<int> left, List<int> right) = SanitizeInput(Input);
+        int sum = 0;
+        foreach (int value in left)
+        {
+            int multiplier = right.Count(x => x == value);
+            sum += value * multiplier;
+        }
+        return sum.ToString();
+    }
+
+}
---
+++ Day1/Tests/Test.cs
@@ -0,0 +1,78 @@
+﻿using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+    [Fact]
+    public void TestInput()
+    {
+        string input = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """;
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part1().ShouldBe("11");
+    }
+
+    [Fact]
+    public void Part2TestInput()
+    {
+        string input = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """;
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("31");
+    }
+
+    [Fact]
+    public void SanitizeInput()
+    {
+        string input = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """;
+        (List<int> first, List<int> second) = Puzzle.SanitizeInput(input);
+        first.Count.ShouldBe(6);
+        second.Count.ShouldBe(6);
+        first[0].ShouldBe(3);
+        second[0].ShouldBe(4);
+        first[1].ShouldBe(4);
+        second[1].ShouldBe(3);
+        first[2].ShouldBe(2);
+        second[2].ShouldBe(5);
+        first[3].ShouldBe(1);
+        second[3].ShouldBe(3);
+        first[4].ShouldBe(3);
+        second[4].ShouldBe(9);
+        first[5].ShouldBe(3);
+        second[5].ShouldBe(3);
+    }
+
+    [Fact]
+    public void Final()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("1882714");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("19437052");
+    }
+}
