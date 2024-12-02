5a65026715d2a39386df1f3feb8a8764eb2bf7f9
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-02 05:34:54 -0700

    feat: Finish Day 2 Part 1

---
+++ Day2/Puzzle/Puzzle.cs
@@ -0,0 +1,42 @@
+﻿namespace CaptainCoder.AdventOfCode;
+
+public class Puzzle
+{
+    public string Input { get; }
+    public string[] Rows { get; }
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+        Rows = Input.Split(Environment.NewLine);
+    }
+
+    public static bool IsSafe(string input)
+    {
+        // [ 7, 6, 4, 2, 1 ]
+        // [ 6, 4, 2, 1]
+        // [ (7, 6), (6, 4), (4, 2), (2, 1)]
+        var values = input.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                                .Select(int.Parse);
+
+        var diffs = values.Zip(values.Skip(1))
+                          .Select(pair => pair.First - pair.Second);
+
+        return diffs.All(x => x >= 1 && x <= 3) || 
+               diffs.All(x => x >= -3 && x <= -1);
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public virtual string Part1()
+    {
+        return Rows.Count(IsSafe).ToString();
+    }
+
+    public virtual string Part2()
+    {
+        return "Unimplemented";
+    }
+}
---
+++ Day2/Tests/Test.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+    [Fact]
+    public void TestInputPart1()
+    {
+        string input = """
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+        """;
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part1().ShouldBe("2");
+    }
+
+    [Theory]
+    [InlineData("7 6 4 2 1", true)]
+    [InlineData("1 2 7 8 9", false)]
+    [InlineData("9 7 6 2 1", false)]
+    [InlineData("1 3 2 4 5", false)]
+    [InlineData("8 6 4 4 1", false)]
+    [InlineData("1 3 6 7 9", true)]
+    public void TestIsSafe(string input, bool isSafe)
+    {
+        Puzzle.IsSafe(input).ShouldBe(isSafe);
+    }
+
+    [Fact]
+    public void FinalPart1()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("332");
+    }
+
+    [Fact]
+    public void TestInputPart2()
+    {
+        string input = """
+
+        """;
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("Some answer");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("Answer Unknown");
+    }
+}
