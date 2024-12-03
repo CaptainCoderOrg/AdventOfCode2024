a57d136caed23f990f54246dfe3412bca4fe973f
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-03 05:10:53 -0700

    chore: Day 3 Part 1

---
+++ Day3/Puzzle/Puzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CaptainCoder.AdventOfCode;
+
+public partial class Puzzle
+{
+    public string Input { get; }
+    public string[] Rows => Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+    [GeneratedRegex("""mul\((?<first>(\d{1,3})),(?<second>(\d{1,3}))\)""", RegexOptions.Compiled, "en-US")]
+    public static partial Regex MulRegex();
+
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public virtual string Part1()
+    {
+        int sum = 0;
+        return MulRegex().Matches(Input).Sum(DoMul).ToString();
+    }
+
+    private static int DoMul(Match match) => int.Parse(match.Groups["first"].Value) * int.Parse(match.Groups["second"].Value);
+
+    public virtual string Part2()
+    {
+        return "Unimplemented";
+    }
+
+}
---
+++ Day3/Tests/Test.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+    [Fact]
+    public void TestInputPart1()
+    {
+        string input = """
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        """;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part1().ShouldBe("161");
+    }
+
+    [Theory]
+    [InlineData("mul(4,5)", true)]
+    [InlineData("mul( 4 , 5 )", false)]
+    [InlineData("mul(4*, mul(6,9!, ?(12,34)", false)]
+    [InlineData("mul(123,321)", true)]
+    [InlineData("mul(1234,321)", false)]
+    [InlineData("mul (123,321)", false)]
+    public void TestMulRegEx(string input, bool isMatch)
+    {
+        Match m = Puzzle.MulRegex().Match(input);
+        m.Success.ShouldBe(isMatch);
+    }
+
+    [Fact]
+    public void TestGroupNames()
+    {
+        string input = "mul(4,5)";
+        Match match = Puzzle.MulRegex().Match(input);
+        match.Groups["first"]?.Value.ShouldBe("4");
+        match.Groups["second"]?.Value.ShouldBe("5");
+    }
+
+    [Fact]
+    public void FinalPart1()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("175700056");
+    }
+
+        [Fact]
+    public void TestInputPart2()
+    {
+        string input = """
+        NOT SET
+        """;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("Some answer");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("Answer Unknown");
+    }
+}
