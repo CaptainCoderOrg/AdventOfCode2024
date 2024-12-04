923c880e24d5a1518983d7ae83c0a9e68ab2a05d
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-04 05:13:12 -0700

    chore: Finished Day 4 Parts 1 and 2

---
+++ Day4/Puzzle/Puzzle.cs
@@ -0,0 +1,96 @@
+﻿namespace CaptainCoder.AdventOfCode;
+
+public class Puzzle
+{
+    public string Input { get; }
+    public string[] Rows => Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public virtual string Part1()
+    {
+        int count = 0;
+        for (int r = 0; r < Rows.Length; r++)
+        {
+            for (int c = 0; c < Rows[r].Length; c++)
+            {
+                if (Rows[r][c] != 'X') { continue; }
+                count += Direction.Directions.Count(d => HasWord("XMAS", r, c, d));
+            }
+        }
+        return count.ToString();
+    }
+
+    public bool HasWord(string word, int sr, int sc, Direction dir)
+    {
+        foreach (char ch in word)
+        {
+            if (sr < 0 || sr >= Rows.Length || sc < 0 || sc >= Rows[sr].Length) { return false; }
+            if (ch != Rows[sr][sc]) { return false; }
+            sr += dir.DR;
+            sc += dir.DC;
+        }
+        return true;
+    }
+
+    public string CornerLetters(int sr, int sc)
+    {
+        if (sr <= 0 || sc <= 0 || sr >= Rows.Length - 1 || sc >= Rows[sr].Length - 1)
+        {
+            return string.Empty;
+        }
+        return $"{Rows[sr-1][sc-1]}{Rows[sr-1][sc+1]}{Rows[sr+1][sc+1]}{Rows[sr+1][sc-1]}";
+    }
+
+    public bool IsWord(string word, int sr, int sc, Direction[] offsets)
+    {
+        foreach (char ch in word)
+        {
+            if (sr < 0 || sr >= Rows.Length || sc < 0 || sc >= Rows[sr].Length) { return false; }
+            if (ch != Rows[sr][sc]) { return false; }
+            
+        }
+        return true;
+    }
+
+    public virtual string Part2()
+    {
+        HashSet<string> words = ["MMSS", "MSSM", "SMMS", "SSMM"];
+        int count = 0;
+        for (int r = 0; r < Rows.Length; r++)
+        {
+            for (int c = 0; c < Rows[r].Length; c++)
+            {
+                if (Rows[r][c] != 'A') { continue; }
+                if (words.Contains(CornerLetters(r, c)))
+                {
+                    count++;
+                }
+            }
+        }
+        return count.ToString();
+    }
+
+}
+
+public record class Direction(int DR, int DC)
+{
+    public static Direction[] Directions = [
+        new Direction(0, -1),
+        new Direction(1, -1),
+        new Direction(1, 0),
+        new Direction(1, 1),
+        new Direction(0, 1),
+        new Direction(-1, 1),
+        new Direction(-1, 0),
+        new Direction(-1, -1),
+    ];
+}
---
+++ Day4/Tests/Test.cs
@@ -0,0 +1,60 @@
+﻿using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+    [Fact]
+    public void TestInputPart1()
+    {
+        string input = """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+        """;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part1().ShouldBe("18");
+    }
+
+    [Fact]
+    public void FinalPart1()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("2603");
+    }
+
+        [Fact]
+    public void TestInputPart2()
+    {
+        string input = """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+        """;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("9");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("1965");
+    }
+}
