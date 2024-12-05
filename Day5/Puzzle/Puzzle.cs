0676bdc1a16f048c843ba14c3b7b7172e455a249
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-05 06:05:01 -0700

    chore: Finish Day 5 Part 1.

---
+++ Day5/Puzzle/Puzzle.cs
@@ -0,0 +1,67 @@
+﻿namespace CaptainCoder.AdventOfCode;
+
+public class Puzzle
+{
+    public string Input { get; }
+    public HashSet<Rule> Rules { get; }
+    public List<List<int>> ListOfManuals { get; }        
+    public string[] Rows => Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+
+        ListOfManuals = 
+            [..
+                Rows.Where(r => r.Contains(','))
+                    .Select(r => new List<int>(r.Split(',').Select(int.Parse)))          
+            ];
+        Rules = 
+            [.. 
+                Rows.Where(r => r.Contains('|'))
+                    .Select(r => r.Split('|'))
+                    .Select(r => new Rule(int.Parse(r[0]), int.Parse(r[1])))
+            ];
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public bool IsOrdered(List<int> pageNumbers)
+    {
+        // [1, 2, 3, 4, 5]
+        // (1, 2), (1, 3), (1, 4), (1, 5) -- All of these must be true
+        // (2, 3), (2, 4), (2, 5),
+        // (3, 4), (3, 5)
+        // (4, 5)
+        for(int ix = 0; ix < pageNumbers.Count - 1; ix++)
+        {
+            int first = pageNumbers[ix];
+            for (int jx = ix + 1; jx < pageNumbers.Count; jx++)
+            {
+                int second = pageNumbers[jx];
+                if (!Rules.Contains(new Rule(first, second)))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public virtual string Part1()
+    {
+        return ListOfManuals.Where(IsOrdered).Select(ls => ls[ls.Count/2]).Sum().ToString();
+    }
+
+    public virtual string Part2()
+    {
+        return "Unimplemented";
+    }
+
+}
+
+
+public record class Rule(int First, int Second);
---
+++ Day5/Tests/Test.cs
@@ -0,0 +1,111 @@
+﻿using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+    public const string SampleInput = 
+        """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+        """;
+    [Fact]
+    public void TestInputPart1()
+    {
+        SampleInput.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(SampleInput);
+        puzzle.Part1().ShouldBe("143");
+    }
+
+    [Fact]
+    public void TestIsOrdered()
+    {
+
+        // 75,47,61,53,29
+        // 97,61,53,29,13
+        // 75,29,13
+        // 75,97,47,61,53
+        // 61,13,29
+        // 97,13,75,29,47   
+        Puzzle puzzle = new (SampleInput);
+        puzzle.IsOrdered(puzzle.ListOfManuals[0]).ShouldBe(true);
+    }
+
+    [Fact]
+    public void TestParseRules()
+    {
+        string input = """
+        47|53
+        97|13
+        97|61
+
+        75,47,61,53,29
+        97,61,53,29,13
+        """;
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Rules.Count.ShouldBe(3);
+        puzzle.Rules.ShouldContain(new Rule(47, 53));
+        puzzle.Rules.ShouldContain(new Rule(97, 13));
+        puzzle.Rules.ShouldContain(new Rule(97, 61));
+
+        puzzle.ListOfManuals.Count.ShouldBe(2);
+        puzzle.ListOfManuals[0].Count.ShouldBe(5);
+        puzzle.ListOfManuals[0].ShouldBe([75, 47, 61, 53, 29]);
+
+        puzzle.ListOfManuals[1].Count.ShouldBe(5);
+        puzzle.ListOfManuals[1].ShouldBe([97,61,53,29,13]);
+    }
+
+    
+
+    [Fact]
+    public void FinalPart1()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("5391");
+    }
+
+        [Fact]
+    public void TestInputPart2()
+    {
+        string input = """
+        NOT SET
+        """;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("Some answer");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("Answer Unknown");
+    }
+}
