2ba11d22a6b546b4f584c76b807b0bfdef93c18a
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-07 06:05:39 -0700

    chore: Finish Day 7 Parts 1 and 2

---
+++ Day7/Puzzle/Puzzle.cs
@@ -0,0 +1,81 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Puzzle
+{
+    public string Input { get; }
+    public string[] Rows => Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public virtual string Part1() =>
+        Rows.Select(CalibrationEquation.Parse)
+            .Where(EquationExtensions.IsValid)
+            .Sum(eq => eq.TestValue).ToString();
+
+    public virtual string Part2() => 
+        Rows.Select(CalibrationEquation.Parse)
+            .Where(EquationExtensions.IsValidWithConcatenation)
+            .Sum(eq => eq.TestValue).ToString();
+
+}
+
+public record CalibrationEquation(long TestValue, List<long> Values)
+{
+    public const char Separator = ':';
+    // TestValue: V0 V1 V2 V3
+    public static CalibrationEquation Parse(string row)
+    {
+        string[] parts = row.Trim().Split(Separator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        (long testValue, List<long> values) = (long.Parse(parts[0]), parts[1].Split(' ').Select(long.Parse).ToList());
+        return new CalibrationEquation(testValue, values);
+    }
+}
+
+public static class EquationExtensions
+{
+    public static bool IsValid(this CalibrationEquation eq, bool useConcat = false) => 
+        IsValid(eq.TestValue, CollectionsMarshal.AsSpan(eq.Values), eq.Values.Count - 1, useConcat);
+
+    public static bool IsValid(this CalibrationEquation eq) => IsValid(eq, false);
+    public static bool IsValidWithConcatenation(this CalibrationEquation eq) => IsValid(eq, true); 
+
+    private static bool IsValid(long target, Span<long> values, int ix, bool useConcat)
+    {
+        // If there are no targets left, we cannot be valid
+        if (ix < 0) { return false; }
+        long lastElement = values[ix];
+        // If there is only one element left and it is our target, it IS valid
+        if (ix == 0) { return target == lastElement; }
+
+        // If we can factor out the lastElement AND the rest is valid, it is valid
+        if (target % lastElement == 0 && IsValid(target / lastElement, values, ix -1, useConcat)) { return true; }
+
+        // If we can subtract out the lastElement AND the rest is valid, it is valid
+        if (target > lastElement && IsValid(target - lastElement, values, ix - 1, useConcat)) { return true; }
+        
+        // Perform concatenation and recurse
+        // "29": 5 290 
+        string tString = target.ToString();
+        string lString = lastElement.ToString();
+        // "2987": 29 8 7
+        // "298": 29 8
+        // Span<long> n = values[7..12];
+        if (useConcat && 
+            tString.Length > lString.Length && 
+            tString.EndsWith(lString) && 
+            IsValid(long.Parse(tString[..^lString.Length]), values, ix - 1, useConcat)) { return true; }
+
+        // Otherwise, this is not valid
+        return false;
+    }
+}
---
+++ Day7/Tests/Test.cs
@@ -0,0 +1,68 @@
+﻿using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+    public const string SampleInput = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
+    """;
+
+    [Fact]
+    public void TestInputPart1()
+    {
+        string input = SampleInput;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part1().ShouldBe("3749");
+    }
+
+    
+    // 3267: 81 40 27
+    // 83: 17 5
+    // 156: 15 6
+    // 7290: 6 8 6 15
+    // 161011: 16 10 13
+    // 192: 17 8 14
+    // 21037: 9 7 18 13
+    // 292: 11 6 16 20
+    [Fact]
+    public void TestEquationParser()
+    {
+        string input = "21037: 9 7 18 13";
+        var eq = CalibrationEquation.Parse(input);
+        eq.TestValue.ShouldBe(21037);
+        eq.Values.ShouldBe([9, 7, 18, 13]);
+    }
+
+    [Fact]
+    public void FinalPart1()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("945512582195");
+    }
+
+    [Fact]
+    public void TestInputPart2()
+    {
+        string input = SampleInput;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("11387");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("271691107779347");
+    }
+}
