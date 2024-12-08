cbff54107b6de2ceac2f7ed256d7590e79ac86c9
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-12-08 06:36:37 -0700

    chore: Finish Day 8 Part 1 with Dictionary.

---
+++ Day8/Puzzle/Puzzle.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Puzzle
+{
+    public const char EMPTY = '.';
+    public string Input { get; }
+    public string[] Rows => Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    
+    public Puzzle(string input)
+    {
+        Input = input.ReplaceLineEndings();
+    }
+
+    public static Puzzle FromFile(string path)
+    {
+        return new Puzzle(File.ReadAllText(path));
+    }
+
+    public virtual string Part1()
+    {
+        // ILookup<char, Position> lookup = FindAntennaIEnumerables(Rows).ToLookup(t => t.Item1, t => t.Item2);
+        // var antinodes = lookup.Select(FindAntinodes);
+        return FindAntennaDictionary(Rows).Values.Select(FindAntinodes).SelectMany(s => s).ToHashSet().Count(InGrid).ToString();
+        bool InGrid(Position p) => InBounds(Rows, p);
+    }
+
+
+    public static bool InBounds(string[] grid, Position p)
+    {
+        return !(p.Row < 0 || p.Col < 0 || p.Row >= grid.Length || p.Col >= grid[p.Row].Length);
+    }
+    
+
+    public static HashSet<Position> FindAntinodes(IEnumerable<Position> antenna)
+    {
+        HashSet<Position> antis = new();
+        foreach (Position p0 in antenna)
+        {
+            foreach (Position p1 in antenna) 
+            {
+                if (p0 == p1) { continue; }
+                antis.UnionWith(FindAntinodes(p0, p1));
+            }
+        }
+        return antis;
+    }
+
+    public static IEnumerable<Position> FindAntinodes(Position p0, Position p1)
+    {
+        yield return new Position(p0.Row - (p1.Row - p0.Row), p0.Col - (p1.Col - p0.Col));
+        yield return new Position(p1.Row + (p1.Row - p0.Row), p1.Col + (p1.Col - p0.Col));
+    }
+
+    public static Dictionary<char, List<Position>> FindAntennaDictionary(string[] grid)
+    {
+        Dictionary<char, List<Position>> antenna = new();
+        foreach (int row  in Enumerable.Range(0, grid.Length))
+        {
+            foreach (int col in Enumerable.Range(0, grid[row].Length))
+            {
+                char symbol = grid[row][col];
+                if (symbol == EMPTY) { continue; }
+                if (!antenna.TryGetValue(symbol, out List<Position>? nodes))
+                {
+                    nodes = new List<Position>();
+                    antenna[symbol] = nodes;
+                }
+                nodes.Add(new Position(row, col));
+            }
+        }
+        return antenna;
+    }
+
+    public static IEnumerable<(char, Position)> FindAntennaIEnumerables(string[] grid)
+    {
+        // Dictionary<char, List<Position>> antenna = new();
+        //new Lookup<char, Position>() { };
+        foreach (int row  in Enumerable.Range(0, grid.Length))
+        {
+            foreach (int col in Enumerable.Range(0, grid[row].Length))
+            {
+                char symbol = grid[row][col];
+                if (symbol == EMPTY) { continue; }
+                yield return (symbol, new Position(row, col));
+                // if (!antenna.TryGetValue(symbol, out List<Position>? nodes))
+                // {
+                //     nodes = new List<Position>();
+                //     antenna[symbol] = nodes;
+                // }
+                // nodes.Add(new Position(row, col));
+            }
+        }
+        // return antenna;
+    }
+
+    public virtual string Part2()
+    {
+        return "Unimplemented";
+    }
+
+}
+
+public record Position(int Row, int Col);
---
+++ Day8/Tests/Test.cs
@@ -0,0 +1,97 @@
+﻿using Shouldly;
+
+namespace CaptainCoder.AdventOfCode;
+
+public class Test
+{
+
+    public const string SampleInput = """
+    ............
+    ........0...
+    .....0......
+    .......0....
+    ....0.......
+    ......A.....
+    ............
+    ............
+    ........A...
+    .........A..
+    ............
+    ............
+    """;
+    [Fact]
+    public void TestInputPart1()
+    {
+        string input = SampleInput;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part1().ShouldBe("14");
+    }
+
+    /*
+               11
+     012345678901
+    0............
+    1........0...
+    2.....0......
+    3.......0....
+    4....0.......
+    5......A.....
+    6............
+    7............
+    8........A...
+    9.........A..
+    0............
+    1............
+    */
+    [Fact]
+    public void TestFindAntenna()
+    {
+        Puzzle puzzle = new (SampleInput);
+        Dictionary<char, List<Position>> antenna = Puzzle.FindAntennaDictionary(puzzle.Rows);
+        antenna.Keys.Count.ShouldBe(2);
+        antenna['0'].Count.ShouldBe(4);
+        antenna['A'].Count.ShouldBe(3);
+        antenna['0'].ShouldContain(new Position(1, 8));
+        antenna['0'].ShouldContain(new Position(2, 5));
+        antenna['0'].ShouldContain(new Position(3, 7));
+        antenna['0'].ShouldContain(new Position(4, 4));
+        antenna['A'].ShouldContain(new Position(5, 6));
+        antenna['A'].ShouldContain(new Position(8, 8));
+        antenna['A'].ShouldContain(new Position(9, 9));
+    }
+
+    [Fact]
+    public void TestFindAntinodes()
+    {
+        Position[] anti = [.. Puzzle.FindAntinodes(new Position(1, 8), new Position(2, 5))];
+        // (3, 2), (1, 11)
+        anti.ShouldContain(new Position(3, 2));
+        anti.ShouldContain(new Position(0, 11));
+    }
+
+    [Fact]
+    public void FinalPart1()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part1().ShouldBe("Answer Unknown");
+    }
+
+        [Fact]
+    public void TestInputPart2()
+    {
+        string input = """
+        NOT SET
+        """;
+        input.Trim().ShouldNotBe("NOT SET");
+        Puzzle puzzle = new Puzzle(input);
+        puzzle.Part2().ShouldBe("Some answer");
+    }
+
+    [Fact]
+    public void FinalPart2()
+    {
+        Puzzle puzzle = Puzzle.FromFile("inputs/puzzle-input.txt");
+        puzzle.Part2().ShouldBe("Answer Unknown");
+    }
+}
